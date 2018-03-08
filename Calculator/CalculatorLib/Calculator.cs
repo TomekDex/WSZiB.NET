acc58eefc924641d7cc0f52492be66369dab89ae
TomekDex <tjtj@poczta.onet.pl>
2018-03-08 18:01:54 +0100

    Ruszamy

---
+++ Calculator/CalculatorLib/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class Calculator : ICalculatable
+    {
+        public string Calculate(string operation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Witam w projekcie WSZiB.NET
+Pierwsze wyznanie jest w solucji Calculator. W projekcie CalculatorLib w klasie Calculator trzeba zmienić metodę Calculate. Tak aby działała na liczbach zmiennoprzecinkowych ze znakiem i potrafiła podstawowe operacje matematyczne. Modyfikuje to na swoich rozgałęzieniach (Branch)
+
