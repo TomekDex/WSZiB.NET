14abfdf7651425392532eae19434f1fbc81bb764
TomekDex <tjtj@poczta.onet.pl>
2018-03-11 13:25:24 +0100

    Exemplary calculate method
    Challenge

--- Calculator/CalculatorLib/Calculator.cs
+++ Calculator/CalculatorLib/Calculator.cs
@@ -1,16 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CalculatorLib
 {
     public class Calculator : ICalculatable
     {
+        Regex exBracket = new Regex(@"\((?<in>[^()]*)\)", RegexOptions.Compiled);
+        Regex exFunction = new Regex(@"(?<function>[a-z]+)\((?<in>[^()]*)\)", RegexOptions.Compiled);
+        Regex exNegative = new Regex(@"(?<=\d)-(?=\d)", RegexOptions.Compiled);
+        Regex exDoubleNegative = new Regex(@"(?<=\d)--(?=\d)", RegexOptions.Compiled);
         public string Calculate(string operation)
         {
-            throw new NotImplementedException();
+            operation = exDoubleNegative.Replace(operation, "+");
+
+            Match mFunction = exFunction.Match(operation);
+            switch (mFunction.Groups["function"].Value)
+            {
+                case "sin":
+                    double sin = Math.Sin(double.Parse(Calculate(mFunction.Groups["in"].Value)));
+                    return Calculate(operation.Replace(mFunction.Value, sin.ToString()));
+                default:
+                    break;
+            }
+
+            Match mBracket = exBracket.Match(operation);
+            if (mBracket.Success)
+                return Calculate(operation.Replace(mBracket.Value, Calculate(mBracket.Groups["in"].Value)));
+
+            if (operation.Contains("+"))
+                return SingleOperationCalculate(operation.Split('+'), "+", (x, y) => x + y);
+            else if (exNegative.IsMatch(operation))
+                return SingleOperationCalculate(exNegative.Split(operation), "-", (x, y) => x - y);
+            else if (operation.Contains("*"))
+                return SingleOperationCalculate(operation.Split('*'), "*", (x, y) => x * y);
+            else if (operation.Contains("/"))
+                return SingleOperationCalculate(operation.Split('/'), "/", (x, y) => x / y);
+
+            return operation;
+        }
+
+        private string SingleOperationCalculate(string[] parts, string operato, Func<double, double, double> operation)
+        {
+            double left = double.Parse(Calculate(parts[0]));
+            double right = double.Parse(Calculate(string.Join(operato, parts, 1, parts.Length - 1)));
+            return operation(left, right).ToString();
         }
     }
 }
