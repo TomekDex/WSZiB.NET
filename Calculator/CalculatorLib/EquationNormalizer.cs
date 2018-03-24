c6b6ec3e7cfda728e9ac8d751c71a376d420c9d0
Niobar <kurunir@gmail.com>
2018-03-24 12:01:52 +0100

    EquationNormalizer updated

--- Calculator/CalculatorLib/EquationNormalizer.cs
+++ Calculator/CalculatorLib/EquationNormalizer.cs
@@ -12,6 +12,10 @@ namespace CalculatorLib
         public static string EqautionNormer(string equation)
         {
             string mEquationPrevious = "";
+            bool endBracketPrevious = false;
+            bool operatorCheckPrevious = false;
+            bool numberCheckPrevious;
+            //all variables ending with Previous are inherited from previous Match in mcEquation
             string normalizedEquation = "";
             int id;/*non used number for TryParse method to work*/
             bool numberCheck;
@@ -19,23 +23,33 @@ namespace CalculatorLib
             MatchCollection mcEquation = exEquationNormalizerLoader.Matches(equation);
             foreach (Match mEquation in mcEquation)
             {
+                bool operatorCheck = false; //bool variable used to reconginze if current match is operator known to WpfCalculator (+, -, * ...)
+                operatorCheckPrevious = false;
+                numberCheck = Int32.TryParse(mEquation.Value, out id);
+                if (mEquation.Value == "-" || mEquation.Value == "+" || mEquation.Value == "*" || mEquation.Value == "/") operatorCheck = true;
+                if (mEquationPrevious == "-" || mEquationPrevious == "+" || mEquationPrevious == "*" || mEquationPrevious == "/") operatorCheckPrevious = true;
+                if (endBracketPrevious == true && operatorCheck == true) endBracketPrevious = false;
+                if (endBracketPrevious == true && numberCheck == true) normalizedEquation = normalizedEquation + "*";
                 switch (mEquation.Value)
                 {
                     case "-":
-                        bool operatorChecker = false;
-                        if (mEquationPrevious == "-" || mEquationPrevious == "+" || mEquationPrevious == "*" || mEquationPrevious == "/") operatorChecker = true;
-                        if (operatorChecker == true) normalizedEquation = normalizedEquation + "@";
-                        if (operatorChecker == false) normalizedEquation = normalizedEquation + mEquation.Value;
+                        if (operatorCheckPrevious == true) normalizedEquation = normalizedEquation + "@";
+                        if (operatorCheckPrevious == false) normalizedEquation = normalizedEquation + mEquation.Value;
                         break;
                     case "(":
-                        if (numberCheck = Int32.TryParse(mEquationPrevious, out id) == true) normalizedEquation = normalizedEquation + "*(";
+                        if (numberCheckPrevious = Int32.TryParse(mEquationPrevious, out id) == true) normalizedEquation = normalizedEquation + "*(";
+                        else normalizedEquation = normalizedEquation + mEquation.Value;
+                        break;
+                    case ")":
+                        normalizedEquation = normalizedEquation + mEquation.Value;
+                        endBracketPrevious = true;
                         break;
                     default:
-                        normalizedEquation = normalizedEquation + mEquation.Value;
+                        if (operatorCheck == true || numberCheck == true) normalizedEquation = normalizedEquation + mEquation.Value;
+                        Console.WriteLine();
                         break;
                 }
-
-                mEquationPrevious = mEquation.Value;
+                if (operatorCheck == true || numberCheck == true) mEquationPrevious = mEquation.Value;
             }
             equation = normalizedEquation;
             return equation;
